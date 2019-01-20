c7811f12778db369e28f8fed9ad3356443aa4d36
JessicaZxt <741776447@qq.com>
2019-01-20 13:11:34 +0800

    2-完成了注册页面

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 //导包
 const express=require('express');
+const path=require('path');
 
 //创建app
 const app=express();
 
-//处理请求
-app.get('/',(req,res)=>{
-    res.send('hello world');
-})
+//设置静态根目录
+app.use(express.static(path.join(__dirname,'public')));
+
+//导入路由对象
+const accountRouter=require(path.join(__dirname,'routers/accountRouter.js'));
+app.use('/account',accountRouter);
+
 
 //启动
 app.listen(8080,'127.0.0.1',err=>{
